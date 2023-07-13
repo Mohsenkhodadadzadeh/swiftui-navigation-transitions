5217fb2c18da65b0033001e776c3b1b606a64e17
David Roman <2538074+davdroman@users.noreply.github.com>
2023-07-13 17:46:10 +0100

    wip

--- Tests/RuntimeSwizzlingTests/RuntimeSwizzlingTests.swift
+++ Tests/RuntimeSwizzlingTests/RuntimeSwizzlingTests.swift
@@ -1,55 +1,52 @@
 import ObjectiveC.runtime
 import UIKit
 
-struct Swizzling {
+typealias ViewDidAppearFunction = @convention(c) (UIViewController, Selector, Bool) -> Void
+typealias ViewDidAppearBlock = @convention(block) (UIViewController, Bool) -> Void
 
-	typealias ViewDidAppearFunction = @convention(c) (UIViewController, Selector, Bool) -> Void
-	typealias ViewDidAppearBlock = @convention(block) (UIViewController, Bool) -> Void
+func _swizzleViewDidAppear(
+	_ class_: AnyClass,
+	to block: @escaping ViewDidAppearBlock
+) -> IMP? {
 
-	static private func swizzleViewDidAppear(
-		_ class_: AnyClass,
-		to block: @escaping ViewDidAppearBlock
-	) -> IMP? {
+	let selector = #selector(UIViewController.viewDidAppear(_:))
+	let method: Method? = class_getInstanceMethod(class_, selector)
+	let newImplementation: IMP = imp_implementationWithBlock(unsafeBitCast(block, to: AnyObject.self))
 
+	if let method = method {
+		let oldImplementation: IMP = method_getImplementation(method)
+		method_setImplementation(method, newImplementation)
+		return oldImplementation
+	} else {
+		class_addMethod(class_, selector, newImplementation, "")
+		return nil
+	}
+}
+
+//func removeViewDidAppearSwizzle(_ class_: AnyClass, originalImplementation: IMP?) {
+//	let selector = #selector(UIViewController.viewDidAppear(_:))
+//	guard let method: Method = class_getInstanceMethod(class_, selector) else { return }
+//	method_setImplementation(method, originalImplementation)
+//}
+
+func swizzleViewDidAppear(
+	viewController: UIViewController,
+	to block: @escaping (UIViewController, Bool) -> Void
+) {
+	var implementation: IMP?
+	let class_ = type(of: viewController)
+	let swizzledBlock: ViewDidAppearBlock = { calledViewController, animated in
 		let selector = #selector(UIViewController.viewDidAppear(_:))
-		let method: Method? = class_getInstanceMethod(class_, selector)
-		let newImplementation: IMP = imp_implementationWithBlock(unsafeBitCast(block, to: AnyObject.self))
-
-		if let method = method {
-			let oldImplementation: IMP = method_getImplementation(method)
-			method_setImplementation(method, newImplementation)
-			return oldImplementation
-		} else {
-			class_addMethod(class_, selector, newImplementation, "")
-			return nil
+		if let implementation = implementation {
+			let viewDidAppear: ViewDidAppearFunction = unsafeBitCast(implementation, to: ViewDidAppearFunction.self)
+			viewDidAppear(calledViewController, selector, animated)
 		}
-	}
-
-//	static private func removeViewDidAppearSwizzle(_ class_: AnyClass, originalImplementation: IMP?) {
-//		let selector = #selector(UIViewController.viewDidAppear(_:))
-//		guard let method: Method = class_getInstanceMethod(class_, selector) else { return }
-//		method_setImplementation(method, originalImplementation)
-//	}
-
-	static func swizzleViewDidAppear(
-		viewController: UIViewController,
-		to block: @escaping (UIViewController, Bool) -> Void
-	) {
-		var implementation: IMP?
-		let class_ = type(of: viewController)
-		let swizzledBlock: ViewDidAppearBlock = { calledViewController, animated in
-			let selector = #selector(UIViewController.viewDidAppear(_:))
-			if let implementation = implementation {
-				let viewDidAppear: ViewDidAppearFunction = unsafeBitCast(implementation, to: ViewDidAppearFunction.self)
-				viewDidAppear(calledViewController, selector, animated)
-			}
-			if viewController === calledViewController {
-				block(viewController, animated)
+		if viewController === calledViewController {
+			block(viewController, animated)
 //				removeViewDidAppearSwizzle(class_, originalImplementation: implementation)
-			}
 		}
-		implementation = swizzleViewDidAppear(class_, to: swizzledBlock)
 	}
+	implementation = _swizzleViewDidAppear(class_, to: swizzledBlock)
 }
 
 import XCTest
@@ -82,7 +79,7 @@ final class RuntimeSwizzlingTests: XCTestCase {
 		expectation2.expectedFulfillmentCount = 1
 
 		let sut = SUT(expectation: expectation1)
-		Swizzling.swizzleViewDidAppear(viewController: sut) { `self`, animated in
+		swizzleViewDidAppear(viewController: sut) { `self`, animated in
 			XCTAssertTrue(animated)
 			expectation2.fulfill()
 		}
